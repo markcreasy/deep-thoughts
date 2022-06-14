72788a417bea68d764f8a255f2ff84a803379a2d
Mark Creasy <mcreasy50@gmail.com>
2022-06-14 16:06:18 -0400

    db updates

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,10 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
+// User ID: 62a8a15dcefdf2951d6a1d88
+// Thought ID: 62a8a15ecefdf2951d6a1f48
+// User name: Geo_Hansen4
+
 const typeDefs = gql`
   type Thought {
     _id: ID
@@ -28,6 +32,27 @@ const typeDefs = gql`
   }
 
   type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
