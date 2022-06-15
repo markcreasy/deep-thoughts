5eb21fe0527509b6918474ff20c3fdf70687a42b
Mark Creasy <mcreasy50@gmail.com>
2022-06-14 22:24:28 -0400

    friendlist

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
@@ -29,6 +30,14 @@ const Profile = () => {
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+        </div>
+
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
         </div>
       </div>
     </div>
