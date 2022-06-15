119d39b0c29e2cc7fa450567c0a71530945d2aca
Mark Creasy <mcreasy50@gmail.com>
2022-06-15 10:09:29 -0400

    update profile page for logged in/out content

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,30 +1,50 @@
-import { useParams } from 'react-router-dom';
-
+// package functions
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+// components
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+// custom utils
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const Profile = () => {
+  // check url string for a username
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  // if there is a username in the url, query user. Else, query Me
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  // set user to me or user based on the query response
+  const user = data?.me || data?.user || {};
 
+  // navigate to personal profile page if username is the logged-in user's
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
+
+  // Show loading message while waiting for data from db
   if (loading) {
     return <div>Loading...</div>;
+  }
+
+  // if no username, and no logged in user, display error message
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
   }
 
   return (
     <div>
       <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
-        </h2>
+      <h2 className="bg-dark text-secondary p-3 display-inline-block">
+        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+      </h2>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
