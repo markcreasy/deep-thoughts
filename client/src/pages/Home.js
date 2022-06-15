9c98fa48ba7c8a6030f98797e9d50f4daf6ed532
Mark Creasy <mcreasy50@gmail.com>
2022-06-15 10:00:37 -0400

    custom home page for logged in users

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,42 @@
-import React from 'react';
+// package imports
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+// custome utils
+import Auth from '../utils/auth';
+//components
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
 const Home = () => {
-  // use useQuery hook to make query request
+  // get thought data
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // attempt to retrieve user data based on jwt auth header
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  // populate thoughts array when data is available
   const thoughts = data?.thoughts || [];
-  console.log(thoughts);
+
+  // check logged in status
+  const loggedIn = Auth.loggedIn();
 
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-          )}
+      <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+        )}
+      </div>
+      {loggedIn && userData ? (
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={userData.me.username}
+            friendCount={userData.me.friendCount}
+            friends={userData.me.friends}
+          />
         </div>
+      ) : null}
       </div>
     </main>
   );
