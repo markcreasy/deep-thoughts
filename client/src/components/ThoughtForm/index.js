a908ec95e458cf181a7502015450af73ce92283e
Mark Creasy <mcreasy50@gmail.com>
2022-06-15 13:21:00 -0400

    reaction form

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,9 +1,38 @@
+// package utils
 import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+// graphql db interactions
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_ME, QUERY_THOUGHTS } from '../../utils/queries';
 
 const ThoughtForm = () => {
   // setup state
   const [thoughtText, setText] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
+
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+
+        // could potentially not exist yet, so wrap in a try/catch
+      try {
+        // update me array's cache
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+        });
+      } catch (e) {
+        console.warn("First thought insertion by user!")
+      }
+
+      // update thought array's cache
+      const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+      cache.writeQuery({
+        query: QUERY_THOUGHTS,
+        data: { thoughts: [addThought, ...thoughts] },
+      });
+    }
+  });
 
   // form state callback
   const handleChange = event => {
@@ -13,17 +42,29 @@ const ThoughtForm = () => {
     }
   };
 
-  // form submit handler
+  // form submission callback
   const handleFormSubmit = async event => {
     event.preventDefault();
-    setText('');
-    setCharacterCount(0);
+
+    try {
+      // add thought to database
+      await addThought({
+        variables: { thoughtText }
+      });
+
+      // clear form value
+      setText('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
       Character Count: {characterCount}/280
+      {error && <span className="ml-2">Something went wrong...</span>}
       </p>
       <form
         className="flex-row justify-center justify-space-between-md align-stretch"
